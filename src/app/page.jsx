adc21e23952ba7110a98cdedf3465fd328fd9c65
evalon <ibsencedev@gmail.com>
2024-03-04 16:40:03 +0200

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,15 +4,18 @@ import { redirect } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [loggedIn, setLoggedIn] = useState(JSON.parse(localStorage.getItem("userData")));
+  const [loggedIn, setLoggedIn] = useState();
   const [loggedOut, setLoggedOut] = useState(null);
   useEffect(() => {
-    console.log("LOGGED IN", loggedIn);
+    setLoggedIn(() => {
+      const userData = localStorage.getItem("userData");
+      return userData ? JSON.parse(userData) : null;
+    });
     if (loggedOut) {
       localStorage.removeItem("userData");
       redirect("/api/auth/login");
     }
-  }, [loggedIn, loggedOut]);
+  }, [loggedOut]);
   if (!loggedIn) {
     return <>NO USER DATA</>;
   } else {
