351481b12d291773d126781511df013e8b8e4c26
Marteniuc Ionel-Nicolae <88239982+1bsence@users.noreply.github.com>
2024-03-14 01:30:18 +0200

    Update page.jsx

--- src/app/department/[id]/page.jsx
+++ src/app/department/[id]/page.jsx
@@ -88,14 +88,11 @@ export default function Page() {
       )
     ) {
       console.log("You do not have permission to view this page");
-      console.log(currUser);
       redirect("/");
     } else {
       console.log("You have permission to view this page");
-      console.log(currUser);
     }
   }
-
   if (router.isFallback || !loggedIn || !department) {
     return (
       <div className="flex flex-col items-center justify-center">
@@ -105,69 +102,77 @@ export default function Page() {
   } else {
     return (
       <div>
-        ADD MANAGER!!
-        {department.manager && <DepManagersList employees={employees} />}
-      </div>
+        {!department.manager.lenght < 2 && (
+          <DepManagersList employees={employees} />
+        )}
 
-      // <div className="py-5">
-      //   {department && (
-      //     <div className="flex flex-col justify-evenly items-center">
-      //       {department?.name}
-      //       {department.manager && (
-      //         <h1>
-      //           Manager: {department?.manager?.name || department?.manager?.id}
-      //         </h1>
-      //       )}
-      //     </div>
-      //   )}
-      //   <div className="flex flex-row justify-between bg-foreground px-4 rounded-sm+ shadow-md">
-      //     <h1>EMPLOYEES:</h1>
-      //     <button type="button">
-      //       <div className="flex +flex-row justify-center">
-      //         <h1 className="mx-2">Edit </h1>
-      //         <Image src={editIcon} alt="add employee" width={20} />
-      //       </div>
-      //     </button>
-      //   </div>
-      //   {employees &&
-      //     (console.log(employees),
-      //     (
-      //       <div className="px-2">
-      //         <ul>
-      //           {employees.map((emp, key) => (
-      //             <li key={key} className="mx-2 my-3">
-      //               <div className="flex flex-row items-center justify-between rounded-md shadow-sm shadow-glow-type1 px-2 min-h-20 bg-foreground hover:bg-background hover:border-glow-type1 hover:border-[0.5px]">
-      //                 <div className="flex flex-row">
-      //                   <Image
-      //                     src={profileImg}
-      //                     priority
-      //                     alt="employee"
-      //                     width={50}
-      //                     height={50}
-      //                   />
-      //                   <div className="felx felx-col items-center justify-start mx-2">
-      //                     <h3 className="text-base">{emp.name}</h3>
-      //                     {emp?.skills.lenght > 0 && (
-      //                       <h3 className="text-sm text-opacity-60">
-      //                         {emp?.skills?.map((skill) => skill.name)}
-      //                       </h3>
-      //                     )}
-      //                   </div>
-      //                 </div>
-      //                 <div></div>
-      //               </div>
-      //             </li>
-      //           ))}
-      //         </ul>
-      //       </div>
-      //     ))}
-      // </div>
+        <div className="py-5">
+          {department && (
+            <div className="flex flex-col justify-evenly items-center">
+              {department?.name}
+              {department.manager && (
+                <h1>
+                  Manager:{" "}
+                  {department?.manager?.name || department?.manager?.id}
+                </h1>
+              )}
+            </div>
+          )}
+          <div className="flex flex-row justify-between bg-foreground px-4 rounded-sm+ shadow-md">
+            <h1>EMPLOYEES:</h1>
+            <button type="button">
+              <div className="flex +flex-row justify-center">
+                <h1 className="mx-2">Edit </h1>
+                <Image src={editIcon} alt="add employee" width={20} />
+              </div>
+            </button>
+          </div>
+          {employees && (
+            <div className="px-2">
+              <ul>
+                {employees?.some((emp, key) => {
+                  emp.roles.includes("Employee") && (
+                    <li key={key} className="mx-2 my-3">
+                      <div className="flex flex-row items-center justify-between rounded-md shadow-sm shadow-glow-type1 px-2 min-h-20 bg-foreground hover:bg-background hover:border-glow-type1 hover:border-[0.5px]">
+                        <div className="flex flex-row">
+                          <Image
+                            src={profileImg}
+                            priority
+                            alt="employee"
+                            width={50}
+                            height={50}
+                          />
+                          <div className="felx felx-col items-center justify-start mx-2">
+                            <h3 className="text-base">{emp?.name}</h3>
+                            {emp?.skills.lenght > 0 && (
+                              <h3 className="text-sm text-opacity-60">
+                                {emp?.skills?.map((skill) => skill?.name)}
+                              </h3>
+                            )}
+                          </div>
+                        </div>
+                        <div></div>
+                      </div>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
     );
   }
 }
 const DepManagersList = (employees) => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const [loggedIn, setLoggedIn] = useState(() => {
+    if (typeof window !== "undefined") {
+      return localStorage.getItem("userData") || null;
+    }
+  });
   const managers = [];
-  console.log(employees.employees);
   employees.employees.map((emp) => {
     if (emp.roles.includes("Department Manager")) {
       managers.push(emp);
@@ -178,19 +183,47 @@ const DepManagersList = (employees) => {
       {managers.length > 0 && (
         <div className="flex flex-row items-center justify-center">
           <h1 className="px-2">Select Manager:</h1>
-          <select
-            onSubmit={() => {
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
               console.log("submitting");
+              try {
+                fetch(endpoint + "/department/modify", {
+                  method: "POST",
+                  headers: {
+                    accept: "application/json",
+                  },
+                  body: JSON.stringify({
+                    organization: {
+                      id: JSON.parse(loggedIn).organization.id,
+                    },
+                    department: {
+                      id: pathname.split("/")[2],
+                      name: searchParams.get("name"),
+                      manager: e.target[0].value,
+                      skills: [],
+                      employees: [],
+                    },
+                  }),
+                })
+                  .then((res) => res.json())
+                  .then((data) => {
+                    console.log(data);
+                  });
+              } catch (err) {
+                console.log(err);
+              }
             }}
-            className="bg-foreground hover:bg-background hover:border-glow-type1 hover:border-[0.5px] rounded-md"
           >
-            {managers.map((manager, index) => (
-              <option key={index} value={manager.id}>
-                {manager.name}
-              </option>
-            ))}
-          </select>
-          <button type="submit">Submit</button>
+            <select className="bg-foreground hover:bg-background hover:border-glow-type1 hover:border-[0.5px] rounded-md">
+              {managers.map((manager, index) => (
+                <option key={index} value={manager.id}>
+                  {manager.name}
+                </option>
+              ))}
+            </select>
+            <button type="submit">Submit</button>
+          </form>
         </div>
       )}
       {managers.length < 1 && (
